167b8404fe9816fc2dbdec9a3d37e565e018b599
sergei <sb@slam.by>
2021-07-25 01:31:37 +0300

    build

--- NewsApp/APICaller.swift
+++ NewsApp/APICaller.swift
@@ -6,3 +6,53 @@
 //
 
 import Foundation
+
+final class APICaller {
+    static let shared = APICaller()
+    
+    struct Constants{
+        static let topHeadingURL = URL(string: "https://newsapi.org/v2/top-headlines?country=RU&apiKey=eda6154a62744b7bbad849130a7f7b6f")
+    }
+    
+    private init(){}
+    
+    public func getTopStories(completition: @escaping (Result<[Article], Error>) -> Void){
+        guard let url = Constants.topHeadingURL else{return}
+        
+        let task = URLSession.shared.dataTask(with: url) {data, _, error in
+            if let error = error{
+                completition(.failure(error))
+            }
+            else if let data = data {
+                do {
+                    let result = try JSONDecoder().decode(APIResponse.self, from: data)
+                    //print("Articles count \(result.articles.count)")
+                    completition(.success(result.articles))
+                }catch{
+                    completition(.failure(error))
+                }
+            }
+        }
+        
+        task.resume()
+        
+    }
+}
+
+//Models
+
+struct APIResponse: Codable{
+    let articles: [Article]
+}
+
+struct Article: Codable{
+    let source: Source
+    let title: String
+    let description: String?
+    let url: String?
+    let urlToImage: String?
+    let publishedAt: String
+}
+struct Source:Codable {
+    let name: String
+}
--- NewsApp/NewsTableViewCell.swift
+++ NewsApp/NewsTableViewCell.swift
@@ -7,17 +7,106 @@
 
 import UIKit
 
+class NewsTableViewCellViewModel{
+    let title:String
+    let subtitle: String
+    let imageURL: URL?
+    var imageData: Data? = nil
+    
+    init(title: String, subtitle:String, imageURL: URL?) {
+        self.title = title
+        self.subtitle = subtitle
+        self.imageURL = imageURL
+    }
+}
+
 class NewsTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    static let identifier: String = "NewsTableViewCell"
+    
+    private let newsTitleLbel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.font = .systemFont(ofSize: 22, weight: .semibold)
+        return label
+    }()
+    
+    private let subtitlelabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.font = .systemFont(ofSize: 17, weight: .light)
+        return label
+    }()
+    
+    private let newsImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.layer.cornerRadius = 6
+        imageView.layer.masksToBounds = true
+        imageView.clipsToBounds = true
+        imageView.backgroundColor = .secondarySystemBackground
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.addSubview(newsTitleLbel)
+        contentView.addSubview(subtitlelabel)
+        contentView.addSubview(newsImageView)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        newsTitleLbel.frame = CGRect(
+            x: 10,
+            y: 0,
+            width: contentView.frame.size.width - 170,
+            height: 70
+        )
+        
+        subtitlelabel.frame = CGRect(
+            x: 10,
+            y: 70,
+            width: contentView.frame.size.width - 170,
+            height: contentView.frame.size.height/2
+        )
+        
+        newsImageView.frame = CGRect(
+            x: contentView.frame.size.width - 150,
+            y: 5,
+            width: 140,
+            height: contentView.frame.size.height - 10
+        )
     }
-
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
+    
+    func configure(with viewModel: NewsTableViewCellViewModel){
+        newsTitleLbel.text = viewModel.title
+        subtitlelabel.text = viewModel.subtitle
+        
+        // Image
+        
+        if let data = viewModel.imageData{
+            newsImageView.image = UIImage(data: data)
+        }
+        else if let url = viewModel.imageURL{
+            //fetch
+            URLSession.shared.dataTask(with: url) { [weak self] data, _, error in
+                guard let data = data, error == nil else{
+                    return
+                }
+                viewModel.imageData = data
+                DispatchQueue.main.async {
+                    self?.newsImageView.image = UIImage(data: data)
+                }
+            }.resume()
+        }
+    }
 }
